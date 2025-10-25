526972221f2b231d5190fce3d73a8f5c8e437b97
Udit Narayan Singh <uditn007@gmail.com>
2025-10-26 01:13:26 +0530

    created the home page almost and agences page created the basic structure

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router'
 const HomeBottomText = () => {
   return (
     <div className='font-[lausanne-500] text-[6vw] mb-3 text-white/95 flex items-center justify-center uppercase gap-10'>
-      <div className='border-[3px] rounded-full leading-[4vw] pt-5 px-7 hover:text-lime-300 duration-100'>
+      <div className='border-[3px] rounded-full leading-[4vw] pt-5 px-7 hover:text-[#D3FD50] duration-100'>
         <Link to='/projects'>Projets</Link>
       </div>
-      <div className='border-[3px] rounded-full leading-[4vw] pt-5 px-7 hover:text-lime-300 duration-100'>
+      <div className='border-[3px] rounded-full leading-[4vw] pt-5 px-7 hover:text-[#D3FD50] duration-100'>
         <Link to='/agence'>Agence</Link>
       </div>
 
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>Agence</div>
+    <div>
+      <div className='bg-white mt-[44vh]' >
+        <h1 className='font-[lausanne-500] text-center text-[20vw] uppercase'>Soixan7e</h1>
+        <h1 className='font-[lausanne-500] text-center text-[20vw] uppercase -mt-48 '>Douze</h1>
+      </div>
+      <div className='bg-white -mt-24 font-bold pl-[40%] pr-8'>
+        <p className='font-[lausanne-300] text-[3rem] leading-12 tracking-wider'> 
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.
+        </p>
+      </div>
+    </div>
   )
 }
 
